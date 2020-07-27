d070b21ea2f666bfcb0fd6c71449474a7540919b
戴林波 <dailinbo@szkingdom.com>
2020-07-27 18:17:27 +0800

    ie11

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 /*
  * @Author: DaiLinBo
  * @Date: 2020-07-16 15:53:29
- * @LastEditTime: 2020-07-25 22:50:18
- * @LastEditors: DaiLinBo
+ * @LastEditTime: 2020-07-25 23:17:07
+ * @LastEditors: Aiden
  * @Description: 
  */ 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import  "react-app-polyfill/ie11";
-import  "react-app-polufill/stable";
+// import  "react-app-polufill/stable";
 // import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
