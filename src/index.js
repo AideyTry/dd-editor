e68d3e402d3060c02ee82361818f482114bee185
jason19901028 <jason_dlb@sina.cn>
2020-09-09 23:53:40 +0800

    delete redundant

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: DaiLinBo
  * @Date: 2020-07-16 15:53:29
- * @LastEditTime: 2020-09-07 10:19:22
+ * @LastEditTime: 2020-09-09 23:52:04
  * @LastEditors: Aiden
  * @Description:
  */
@@ -12,7 +12,6 @@ import "antd/dist/antd.css";
 import Node from "@/components";
 import Header from "@/components/Header";
 import data from "@/data.json";
-// import Index from '../dist/index.js'
 function TreeNode(props) {
   const dataTree = props.dataTree || data
   const canvasRef = useRef(null);
